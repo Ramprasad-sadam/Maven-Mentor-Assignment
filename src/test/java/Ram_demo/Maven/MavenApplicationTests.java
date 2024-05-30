319500a05e58299dbd3eb5cd12e0b21c92933e4e
ramprasad <ramprasad@gmail.com>
2024-05-30 21:18:08 +0530

    This is source code

---
+++ src/test/java/Ram_demo/Maven/MavenApplicationTests.java
@@ -0,0 +1,13 @@
+package Ram_demo.Maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
